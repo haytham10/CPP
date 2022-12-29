74ae987a7a032365dfa7bc6ce38e16c3811533c2
haytham10 <cnarabi@gmail.com>
2022-12-29 11:38:18 +0100

    EX01 | DONE

--- cpp00/ex01/phonebook.cpp
+++ cpp00/ex01/phonebook.cpp
@@ -10,38 +10,129 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <iomanip>
 #include "phonebook.hpp"
 
-int	main()
-{
-	Phonebook	phonebook;
-	std::string	input;
-	int			i;
+// Add a contact to the phonebook
+void PhoneBook::Add(const Contact& contact) {
+  // If the phonebook is full, replace the oldest contact
+  if (num_contacts == 8) {
+    // Shift all the contacts one position to the left
+    for (int i = 0; i < 7; ++i) {
+      contacts[i] = contacts[i + 1];
+    }
+    // Add the new contact to the end
+    contacts[7] = contact;
+  }
+  // Otherwise, just add the new contact
+  else {
+    contacts[num_contacts++] = contact;
+  }
+}
 
-	std::cout << "Welcome to the phonebook" << std::endl;
-	std::cout << "Please enter a command: " << std::endl;
-	i = 0;
-	while (1)
-	{
-		std::cin >> input;
-		if (input == "")
-			continue ;
-		else
-		{
-			if (input == "EXIT")
-				return (0);
-			else if (input == "ADD")
-			{
-				if (i == 7)
-					i = 0;
-				phonebook.ft_add(i++);
-			}
-			else if (input == "SEARCH")
-				phonebook.ft_search(i);
-			else
-				std::cout << "Invalid command" << std::endl;
-		}
-		std::cout << "Please enter a command: ";
-	}
-	return (0);
+// Search for a contact with a given index
+Contact PhoneBook::Search(int index) const {
+  // If the index is out of range, return an empty contact
+  if (index < 0 || index >= num_contacts) {
+    return Contact("", "", "", "", "");
+  }
+  // Otherwise, return the contact with the given index
+  return contacts[index];
+}
+
+// Truncate a string to a certain length, replacing the last character with a dot (.) if necessary.
+std::string Truncate(const std::string& str, int length) {
+  if (str.length() <= length) {
+    return str;
+  }
+  std::string truncated = str.substr(0, length - 1);
+  truncated += ".";
+  return truncated;
+}
+
+// Display the phonebook
+void PhoneBook::Display() const {
+  // Print the header
+  std::cout << "|   index|first name| last name|nickname  |" << std::endl;
+  // Print a line separator
+  std::cout << "|--------|----------|----------|----------|" << std::endl;
+
+  // Iterate over the contacts
+  for (int i = 0; i < 8; ++i) {
+    // Print the index, right-aligned and padded with spaces
+    std::cout << "|" << std::right << std::setw(8) << i << "|";
+    // Print the first name, right-aligned and padded with spaces
+	std::cout << std::right << std::setw(10) << Truncate(contacts[i].FirstName(), 10) << "|";
+    // Print the last name, right-aligned and padded with spaces
+    std::cout << std::right << std::setw(10) << Truncate(contacts[i].LastName(), 10) << "|";
+    // Print the nickname, right-aligned and padded with spaces
+    std::cout << std::right << std::setw(10) << Truncate(contacts[i].Nickname(), 10) << "|";
+    std::cout << std::endl;
+  }
+}
+
+// Display a contact
+void Contact::Display() const {
+  std::cout << "First name: " << first_name << std::endl;
+  std::cout << "Last name: " << last_name << std::endl;
+  std::cout << "Nickname: " << nickname << std::endl;
+  std::cout << "Phone number: " << phone_number << std::endl;
+  std::cout << "Darkest secret: " << darkest_secret << std::endl;
+}
+
+int main() {
+  // Create a phonebook
+  PhoneBook phonebook;
+
+  while (true) {
+    // Prompt for a command
+    std::string command;
+    std::cout << "Enter a command (ADD, SEARCH, EXIT): ";
+    std::cin >> command;
+
+    // Convert the command to uppercase
+    std::transform(command.begin(), command.end(), command.begin(), ::toupper);
+
+    // If the user enters ADD, prompt for the fields of the new contact and add it to the phonebook
+    if (command == "ADD") {
+      // Prompt for the fields of the new contact
+      std::string first_name, last_name, nickname, phone_number, darkest_secret;
+      std::cout << "Enter the first name: ";
+      std::cin >> first_name;
+      std::cout << "Enter the last name: ";
+      std::cin >> last_name;
+      std::cout << "Enter the nickname: ";
+      std::cin >> nickname;
+      std::cout << "Enter the phone number: ";
+      std::cin >> phone_number;
+      std::cout << "Enter the darkest secret: ";
+      std::cin >> darkest_secret;
+      // Create a new contact with the fields
+      Contact contact(first_name, last_name, nickname, phone_number, darkest_secret);
+      // Add the contact to the phonebook
+      phonebook.Add(contact);
+    }
+    // If the user enters SEARCH, display the saved contacts and prompt for the index of the contact to display
+    else if (command == "SEARCH") {
+      // Display the phonebook
+      phonebook.Display();
+      // Prompt for the index of the contact to display
+      int index;
+      std::cout << "Enter the index of the contact to display: ";
+      std::cin >> index;
+      // Search for the contact with the given index
+      Contact contact = phonebook.Search(index);
+      // Display the contact
+      contact.Display();
+    }
+    // If the user enters EXIT, exit the program
+    else if (command == "EXIT") {
+      // Exit the program
+      return 0;
+    }
+    // Otherwise, continue prompting for commands
+  }
 }
--- cpp00/ex01/phonebook.hpp
+++ cpp00/ex01/phonebook.hpp
@@ -12,66 +12,72 @@
 
 #include <iostream>
 #include <iomanip>
-#include <string>
 
 #ifndef PHONEBOOK_HPP
 # define PHONEBOOK_HPP
 
-class Phonebook
-{
-	private:
-		class Contact
-		{
-			public:
-				std::string	f_name;
-				std::string	l_name;
-				std::string	nickname;
-				std::string	number;
-				std::string	darkest_secret;
-		};
-	public:
-		void	ft_add(int id)
-		{
-			Contact cnt[id];
+// Phonebook contact
+class Contact {
+private:
+  std::string first_name;
+  std::string last_name;
+  std::string nickname;
+  std::string phone_number;
+  std::string darkest_secret;
 
-			std::cout << "First name: ";
-			std::cin >> cnt[id].f_name;
-			std::cout << "Last name: ";
-			std::cin >> cnt[id].l_name;
-			std::cout << "Nickname: ";
-			std::cin >> cnt[id].nickname;
-			std::cout << "Phone number: ";
-			std::cin >> cnt[id].number;
-			std::cout << "Darkest secret: ";
-			std::cin >> cnt[id].darkest_secret;
-			std::cout << "Contact added successfully";
-		}
-		void	ft_search(int id)
-		{
-			Contact	cnt[id];
-			
-			std::cout << "Index     |First Name|Last Name |Nickname  " << std::endl;
-			std::cout << "----------|----------|----------|----------" << std::endl;
-			for (int i = 1; i < id; i++)
-			{
-				std::cout << std::setw(10) << i << "|";
-				std::cout << std::setw(10) << cnt[id].f_name << "|";
-				std::cout << std::setw(10) << cnt[id].l_name << "|";
-				std::cout << std::setw(10) << cnt[id].nickname << std::endl;
-			}
-			std::cout << "Please enter the index of the contact you want to see: ";
-			std::cin >> id;
-			if (id > 7 || id < 0 || isdigit(id) == 0)
-				std::cout << "Invalid index" << std::endl;
-			else
-			{
-				std::cout << "First name: " << cnt[id].f_name << std::endl;
-				std::cout << "Last name: " << cnt[id].l_name << std::endl;
-				std::cout << "Nickname: " << cnt[id].nickname << std::endl;
-				std::cout << "Phone number: " << cnt[id].number << std::endl;
-				std::cout << "Darkest secret: " << cnt[id].darkest_secret << std::endl;
-			}
-		}
+public:
+  // Default constructor
+  Contact() : first_name(""), last_name(""), nickname(""), phone_number(""), darkest_secret("") {}
+  
+  // Constructor
+  Contact(const std::string& first_name, const std::string& last_name,
+          const std::string& nickname, const std::string& phone_number,
+          const std::string& darkest_secret)
+      : first_name(first_name),
+        last_name(last_name),
+        nickname(nickname),
+        phone_number(phone_number),
+        darkest_secret(darkest_secret) {}
+
+  // Get the first name of the contact
+  std::string FirstName() const { return first_name; }
+
+  // Get the last name of the contact
+  std::string LastName() const { return last_name; }
+
+  // Get the nickname of the contact
+  std::string Nickname() const { return nickname; }
+
+  // Get the phone number of the contact
+  std::string PhoneNumber() const { return phone_number; }
+
+  // Get the darkest secret of the contact
+  std::string DarkestSecret() const { return darkest_secret; }
+
+  // Display the contact
+  void Display() const;
+};
+
+// Phonebook
+class PhoneBook {
+private:
+  // Array of contacts
+  Contact contacts[8];
+  // Current number of contacts
+  int num_contacts;
+
+public:
+  // Constructor
+  PhoneBook() : num_contacts(0) {}
+
+  // Add a contact to the phonebook
+  void Add(const Contact& contact);
+
+  // Search for a contact with a given index
+  Contact Search(int index) const;
+
+  // Display the phonebook
+  void Display() const;
 };
 
 #endif
